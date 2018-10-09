28bd0506576c36b6ef7156ea34d80aa05e1cf75d
zj <zj@webturing.com>
2018-10-09 11:12:22 +0800

    input r and calculator area and perimeter

--- lec02/circle.cpp
+++ lec02/circle.cpp
@@ -1,8 +1,13 @@
-#include<iostream>
+#include <bits/stdc++.h>
 using namespace std;
-int main()
-{
-    
-    system("PAUSE");
-    return 0;
+int main() {
+  double pi = 3.1415926;
+  double r;
+  double S, P;
+  cin >> r;
+  S = pi * r * r;
+  P = 2 * pi * r;
+  cout << fixed << setprecision(2) << S << endl;
+  cout << fixed << setprecision(2) << P << endl;
+  return 0;
 }
