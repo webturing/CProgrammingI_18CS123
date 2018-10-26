0f2fbc8af6c1aa130bb3dd9b490d6617d73c5552
ZHAO Jing <ceo@webturing.com>
2018-10-26 15:43:55 +0800

    Add files via upload

---
+++ lec06/primer.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+	int n;cin>>n;
+	int flag=0;
+	for(int i=2;i*i<=n;i++){
+		if(n%i==0){
+			flag=1;
+			break;
+		}
+	}
+	if(flag==0)
+		cout<<n<<endl;
+	return 0;
+}
---
+++ lec06/sum1~n.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+	int n;cin>>n;
+	int s=0;
+	for(int i=1;i<=n;i++){
+		s+=i;
+	}
+	cout<<s<<endl;
+	return 0;
+}
