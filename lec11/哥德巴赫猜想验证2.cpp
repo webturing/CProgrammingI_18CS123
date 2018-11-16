8f800f09b2a79a8b4c4ff8e71326debb019bce94
ZHAO Jing <ceo@webturing.com>
2018-11-16 15:42:53 +0800

    Add files via upload

---
+++ lec11/哥德巴赫猜想验证2.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<cmath>
+#include<fstream>
+using namespace std;
+int prime(int n) {
+	if(n==1)return 0;
+	for(int c=2; c*c<=n; c++) {
+		if(n%c==0) {
+			return 0;
+		}
+	}
+	return 1;
+}
+bool check(int n) {
+	for(int p=3; p<=n/2; p+=2) {
+		if(prime(p)&&prime(n-p))
+			return true;
+	}
+	return false;
+}
+int main() {
+
+	for(int n=6; n<=20000000; n+=2) {	//n=p+(n-p)
+		if(check(n)==false) {
+			cout<<"Congratulations!"<<endl;
+		}
+	}
+
+	return 0;
+}
