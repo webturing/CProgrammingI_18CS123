ebd4076fff229233619759310ad9fddea8c72a12
zj <zj@webturing.com>
2018-11-19 18:11:54 +0800

    lec12 function2

---
+++ lec12/swap1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+void Swap(int &a,int &b ){
+	int t=a;
+	a=b;
+	b=t;
+}
+int main() {
+	int a=3,b=4;
+	std::cout<<a<<" "<<b<<std::endl;
+	Swap(a,b);
+	std::cout<<a<<" "<<b<<std::endl;
+	return 0;
+}
