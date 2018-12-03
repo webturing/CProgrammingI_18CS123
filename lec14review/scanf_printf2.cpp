d9529ea8a9584e9ff8d0ade8b2b0facde5852e67
ZHAO Jing <ceo@webturing.com>
2018-12-03 17:45:44 +0800

    Add files via upload

---
+++ lec14review/scanf_printf2.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+	for(int a=16;; a+=10) {
+		char s[100];
+		sprintf(s,"%d%d",a%10,a/10);
+		int b;
+		sscanf(s,"%d",&b);
+		if(b==4*a) {
+			printf("%d %d",a,b);
+			break;
+		}
+	}
+	return 0;
+}
---
+++ lec14review/sscanf_sprintf.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int a,b;
+	scanf("%d%d",&a,&b);
+	printf("%d\n",a+b);
+	return 0;
+}
